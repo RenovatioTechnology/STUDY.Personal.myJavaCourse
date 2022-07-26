e7a6f1ff2e952ddaf2b2ac6836352e0e26c2d3d6
Jefferson Nah Jr <renovatio3d@gmail.com>
2022-07-25 21:35:12 -0400

    Initial Commit

---
+++ src/cs/study_java/HelloWorld.java
@@ -0,0 +1,11 @@
+package cs.study_java;
+
+public class HelloWorld {
+    public static void main(String[] args){
+
+       // Scanner scnr = new Scanner(System.in);
+
+        System.out.print("Hello World");
+            }
+}
+
