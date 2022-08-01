b6da48beb449f2a60e2e83cd1138dd91820c3d5e
mysql-python-cnnet <Renovatio3d@gmail.com>
2022-08-01 13:58:44 -0400

    Finding the area of a circle from the circumference

---
+++ src/cs/study_java/CombiningStrings.java
@@ -0,0 +1,24 @@
+package cs.study_java;
+// Assign secretID with firstName, a space, and lastName.
+//Ex: If firstName is Barry and lastName is Allen, then output is:
+// Barry Allen
+import java.util.Scanner;
+
+public class CombiningStrings {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        String secretID;
+        String firstName;
+        String lastName;
+
+        firstName = scnr.next();
+        lastName = scnr.next();
+
+        secretID = firstName;
+        char spaceChar = ' ';
+        secretID = secretID + spaceChar;
+        secretID = secretID.concat(lastName);
+
+        System.out.println(secretID);
+    }
+}
