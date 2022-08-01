c6cb6d8444ef09a5f038c2ef79a3ee805a650368
mysql-python-cnnet <Renovatio3d@gmail.com>
2022-08-01 14:46:48 -0400

    Coordinate Geometry

---
+++ src/cs/study_java/DetectWord.java
@@ -0,0 +1,21 @@
+package cs.study_java;
+
+import java.util.Scanner;
+
+public class DetectWord {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        String userString;
+
+        userString = scnr.next();
+
+        if (userString.equals("Quit")) {
+
+            System.out.println("Goodbye");
+
+        } else {
+            System.out.println("Hello");
+        }
+
+    }
+}
